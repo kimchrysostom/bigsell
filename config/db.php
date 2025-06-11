f7b59909b5c84e6834292d3e3c66364b56834ee2
Nicholas Kekena <your-email@example.com>
2025-06-11 21:58:21 +0300

    fixed config

--- config/db.php
+++ config/db.php
@@ -1,10 +1,20 @@
-
 <?php
-$db_host = 'localhost';
-$db_name = 'bigsell';
-$db_user = 'root';
-$db_pass = '';
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    // Local XAMPP settings
+    $db_host = 'localhost';
+    $db_name = 'bigsell';
+    $db_user = 'root';
+    $db_pass = '';
+} else {
+    // Truehost settings
+    $db_host = 'localhost';
+    $db_name = 'vrytxswq_bigisell';
+    $db_user = 'vrytxswq';
+    $db_pass = 'bigisell@selinanasike';
+}
+
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
